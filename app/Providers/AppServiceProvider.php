a797071df28b68a274e33f2a938192330f7321cc
infinitypaul <infinitypaul@live.com>
2019-10-17 09:42:06 +0100

    https url

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Routing\UrlGenerator;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -21,9 +22,11 @@ class AppServiceProvider extends ServiceProvider
     /**
      * Bootstrap any application services.
      *
+     * @param \Illuminate\Routing\UrlGenerator $url
+     *
      * @return void
      */
-    public function boot()
+    public function boot(UrlGenerator $url)
     {
         if ($this->app->environment() == 'production') {
             $url->forceScheme('https');
